0699d8d508faf9e86cb50c2e8569cdbc40b05215
andyosuna <andyosuna@gmail.com>
2016-04-28 15:17:10 -0600

    adds success url to registration view

--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import FormView, TemplateView
 from paypal.standard.forms import PayPalPaymentsForm
 
@@ -8,24 +8,26 @@ from .forms import RegistrationForm
 class RegistrationFormView(FormView):
     template_name = 'registration/form.html'
     form_class = RegistrationForm
+    success_url = reverse_lazy('registration:registration_payment_form')
 
 
 class RegistrationPaymentFormView(FormView):
     template_name = 'registration/payment_form.html'
     form_class = PayPalPaymentsForm
+    success_url = reverse_lazy('registration:success')
 
-    def get_form_kwargs(self):
-        kwargs = super(RegistrationPaymentFormView, self).get_form_kwargs()
-        kwargs.update({
-            'business': 'receiver_email@example.com',
-            'amount': '10000',
-            'item_name': 'IMA Championship Registration',
-            'invoice': 'unique-invoice-id',
-            'notify_url': 'https://example.com' + reverse('paypal-ipn'),
-            'return_url': 'https://example.com/your-return-location',  # success page?
-            'cancel_return': 'https://example.com/your-cancel-location',  # cancel pmt page
-        })
-        return kwargs
+    # def get_form_kwargs(self):
+    #     kwargs = super(RegistrationPaymentFormView, self).get_form_kwargs()
+    #     kwargs.update({
+    #         'business': 'receiver_email@example.com',
+    #         'amount': '10000',
+    #         'item_name': 'IMA Championship Registration',
+    #         'invoice': 'unique-invoice-id',
+    #         'notify_url': 'https://example.com' + reverse('paypal-ipn'),
+    #         'return_url': 'https://example.com/your-return-location',  # success page?
+    #         'cancel_return': 'https://example.com/your-cancel-location',  # cancel pmt page
+    #     })
+    #     return kwargs
 
 
 class DisclaimerTemplateView(TemplateView):
